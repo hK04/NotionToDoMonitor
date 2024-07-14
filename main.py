09248abad14495dc7fc20494a92497ed51aebde0
Matvei Anoshin <matvey.anoshin@inbox.ru>
2024-07-14 15:49:58 +0300

    Initial upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import requests, json 
+import pandas as pd
+import numpy as np
+
+with open('settings.json') as f:
+    settings = json.load(f)
+
+headers = {
+    "Authorization": "Bearer " + settings["token"],
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-02-22"
+}
+
+def readEntireDatabase(databaseID, headers):
+    url = f"https://api.notion.com/v1/databases/{databaseID}/query"
+    page_size = 100 
+    payload = {"page_size": page_size}
+    
+    response = requests.request('POST', url, json=payload, headers=headers)
+    data = response.json()
+    results = data["results"]
+    
+    while data["has_more"]:
+        payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
+        response = requests.request("POST", url, json=payload, headers=headers)
+        data = response.json()
+        results.extend(data["results"])
+    
+    return results
+
+
+if __name__ == "__main__":
+    data = readEntireDatabase(settings["databaseID"], headers)
+    properties = data[0]['properties'].keys()  #properties list
+
+    print(f'|Downloaded {len(data)} positions|')
+    #print('Selected features example:')
+    creation_times, edition_times, names, statuses, tags, dates = [], [], [], [], [], []
+    for page in data:
+        creation_times.append(page['created_time'])   
+        edition_times.append(page['last_edited_time']) 
+
+        if not page['in_trash']:
+            try:
+                names.append(page['properties']['Name']['title'][0]['plain_text']) 
+            except:
+                names.append('')
+            
+            try:
+                statuses.append(page['properties']['Status']['select']['name']) 
+            except:
+                statuses.append('')
+
+            try:
+                tags.append(page['properties']['Tags']['multi_select'])
+            except:
+                tags.append('')
+            
+            try:
+                dates.append(page['properties']['Date']['date'])
+            except:
+                dates.append('')
+
+    db = pd.DataFrame({'CREATION_TIME' : creation_times, 'EDITION_TIME' : edition_times, 
+                        'NAME' : names, 'STATUS' : statuses, 'TAG' : tags, 'DATE' : dates})
+    db.to_csv('data.csv')

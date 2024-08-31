e7a19977e60d95665a71123ccd9f3ab9378429a4
Matvei Anoshin <matvey.anoshin@inbox.ru>
2024-08-31 15:10:42 +0300

    Addition of contne inner part

--- main.py
+++ main.py
@@ -32,19 +32,24 @@ def readEntireDatabase(databaseID, headers):
 if __name__ == "__main__":
     data = readEntireDatabase(settings["databaseID"], headers)
     properties = data[0]['properties'].keys()  #properties list
-
     print(f'|Downloaded {len(data)} positions|')
     #print('Selected features example:')
-    creation_times, edition_times, names, statuses, tags, dates = [], [], [], [], [], []
+    creation_times, edition_times, names, statuses, tags, dates, content = [], [], [], [], [], [], []
     for page in data:
         creation_times.append(page['created_time'])   
         edition_times.append(page['last_edited_time']) 
 
         if not page['in_trash']:
+            #print(page)
             try:
                 names.append(page['properties']['Name']['title'][0]['plain_text']) 
             except:
                 names.append('')
+                
+            try:
+                content.append(page['properties']['Name']['title'][1]['text']['content'])
+            except:
+                content.append('')
             
             try:
                 statuses.append(page['properties']['Status']['select']['name']) 
@@ -60,7 +65,8 @@ if __name__ == "__main__":
                 dates.append(page['properties']['Date']['date'])
             except:
                 dates.append('')
+                
 
     db = pd.DataFrame({'CREATION_TIME' : creation_times, 'EDITION_TIME' : edition_times, 
-                        'NAME' : names, 'STATUS' : statuses, 'TAG' : tags, 'DATE' : dates})
+                        'NAME' : names, 'STATUS' : statuses, 'TAG' : tags, 'DATE' : dates, 'CONTENT' : content})
     db.to_csv('data.csv')
